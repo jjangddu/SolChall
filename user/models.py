0f1ca6c3bd00883906d57d28dff826fac24f1836
jjangddu <peterjdw@naver.com>
2024-02-14 15:30:27 +0900

    [fix] user models

--- user/models.py
+++ user/models.py
@@ -6,6 +6,14 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+
+class Floor(models.Model):
+    floor = models.IntegerField()
+    section = models.AutoField()
+    floor_name = models.CharField(max_length=30)
+
+
 
     def __str__(self):
         return self.username
